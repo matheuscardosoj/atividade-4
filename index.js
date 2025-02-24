755e0cc960e661b1e579a690cf4db1ee8e8ad7e6
Matheus <matheuscardosoj@outlook.com>
2025-02-24 19:36:52 -0300

    adicionando funcionalidade para o usuário digitar via terminal

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
+import readline from 'readline';
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
 function somaDoisNumeros(a, b) {
     return a + b;
 }
+
+function input(query) {
+    return new Promise(resolve => rl.question(query, resolve));
+}
+
+const num1 = await input('Digite o primeiro número: ');
+const num2 = await input('Digite o segundo número: ');
+const resultado = somaDoisNumeros(parseFloat(num1), parseFloat(num2));
+console.log(`A soma dos dois números é: ${resultado}`);
+rl.close();
