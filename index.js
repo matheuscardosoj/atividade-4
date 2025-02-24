d76d0081ae2ebca5c4d5b83adbc2199e166c7526
Matheus <matheuscardosoj@outlook.com>
2025-02-24 19:55:40 -0300

    implementando a limpeza de tela

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import readline from 'readline';
+import os from 'os';
 
 const rl = readline.createInterface({
     input: process.stdin,
@@ -13,7 +14,16 @@ function input(query) {
     return new Promise(resolve => rl.question(query, resolve));
 }
 
+function clearConsole() {
+    if (os.platform() === 'win32') {
+        process.stdout.write('\x1Bc');
+    } else {
+        process.stdout.write('\x1B[2J\x1B[0f');
+    }
+}
+
 while (true) {
+    clearConsole();
     const num1 = await input('Digite o primeiro número: ');
     const num2 = await input('Digite o segundo número: ');
     const resultado = somaDoisNumeros(parseFloat(num1), parseFloat(num2));
