a8615cddba9e7b3ec51addb6707c869fbd28a9ce
Matheus <matheuscardosoj@outlook.com>
2025-02-24 19:41:16 -0300

    adicionando um loob para o usuário somar quantas vezes quiser

--- index.js
+++ index.js
@@ -13,8 +13,15 @@ function input(query) {
     return new Promise(resolve => rl.question(query, resolve));
 }
 
-const num1 = await input('Digite o primeiro número: ');
-const num2 = await input('Digite o segundo número: ');
-const resultado = somaDoisNumeros(parseFloat(num1), parseFloat(num2));
-console.log(`A soma dos dois números é: ${resultado}`);
+while (true) {
+    const num1 = await input('Digite o primeiro número: ');
+    const num2 = await input('Digite o segundo número: ');
+    const resultado = somaDoisNumeros(parseFloat(num1), parseFloat(num2));
+    console.log(`A soma dos dois números é: ${resultado}`);
+    const continuar = await input('Deseja somar outros números? (s/n): ');
+    if (continuar.toLowerCase() !== 's') {
+        break;
+    }
+}
+
 rl.close();
